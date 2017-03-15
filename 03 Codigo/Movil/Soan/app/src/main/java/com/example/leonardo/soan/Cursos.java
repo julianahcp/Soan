38ed970760528c2917ce0a4c885c8a0b11f46647
Leo <lionetonoleo@gmail.com>
2017-03-15 16:50:47 -0600

    soan con tabLayout

--- 03 Codigo/Movil/Soan/app/src/main/java/com/example/leonardo/soan/Cursos.java
+++ 03 Codigo/Movil/Soan/app/src/main/java/com/example/leonardo/soan/Cursos.java
@@ -1,8 +1,11 @@
 package com.example.leonardo.soan;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Leonardo on 23/02/2017.
@@ -12,6 +15,15 @@ public class Cursos extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.asignacion);
+        Button mButonCurso = (Button)findViewById(R.id.buttonUnidad);
+        mButonCurso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent I = new Intent(getApplicationContext(),TestJava.class);
+                startActivity(I);
+            }
+        });
     }
 }
--- 03 Codigo/Movil/Soan/app/src/main/java/com/example/leonardo/soan/MainActivity.java
+++ 03 Codigo/Movil/Soan/app/src/main/java/com/example/leonardo/soan/MainActivity.java
@@ -1,24 +1,42 @@
 package com.example.leonardo.soan;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.zip.Inflater;
+
+import static java.security.AccessController.getContext;
+
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-       final Intent i = new Intent(this,Cursos.class);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        final Intent i = new Intent(this, Cursos.class);
         setContentView(R.layout.activity_main);
 
-        Button buttonCurso = (Button)findViewById(R.id.buttonCurso);
+        Button buttonCurso = (Button) findViewById(R.id.buttonCurso);
         buttonCurso.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -27,5 +45,44 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
